d6a741939d8b4c3c06aa6128d75daae090499e54
Kulbhushan <kulbhushan.singh12@gmail.com>
2016-04-05 15:21:41 +0530

    first commit

---
+++ netcrawler/src/main/java/com/imaginea/netcrawler/App.java
@@ -0,0 +1,16 @@
+package com.imaginea.netcrawler;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("App.main()");
+        
+    }
+}
